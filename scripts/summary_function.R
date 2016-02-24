64bd48f4b3edda3bd3efcc3f08c5379cb78641cd
Matt-Sundsten <mattsund@uw.edu>
2016-02-23 17:29:48 -0800

    Changed summary_function.R to return a list instead of a data frame close #4

--- scripts/summary_function.R
+++ scripts/summary_function.R
@@ -1,5 +1,5 @@
 #The following function takes in a data_frame containing information about students survey before
-#taking info 498f. It will return a data_frame with information about the original data_frame.
+#taking info 498f. It will return a list with information about the original data_frame.
 write_summary <- function(data) {
   num_freshman <- sum(as.vector(data$What.is.your.current.class.standing.) == "Freshman")
   num_sophomore <- sum(as.vector(data$What.is.your.current.class.standing.) == "Sophomore")
@@ -8,7 +8,7 @@ write_summary <- function(data) {
                     == "Yes")
   num_mac <- sum(as.vector(data$What.operating.system.do.you.typically.use.) == "Mac")
   num_windows <- sum(as.vector(data$What.operating.system.do.you.typically.use.) == "Windows")
-  return(data_frame(num_freshman = num_freshman, num_sophomore = num_sophomore, 
+  return(list(num_freshman = num_freshman, num_sophomore = num_sophomore, 
                     num_junior = num_junior, num_interested = num_interested, 
                     num_mac = num_mac, num_windows = num_windows))
 }
