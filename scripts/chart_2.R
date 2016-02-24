d0a627914f4c4e2b100317a0d539fca7358c8ea6
Mary Tong <askmarytong@gmail.com>
2016-02-24 00:44:54 -0800

    first commit

---
+++ scripts/chart_2.R
@@ -0,0 +1,26 @@
+library(jsonlite)
+library(plotly)
+library(dplyr)
+
+# Chart Two
+# The total amount of students who identified their programming level experience in INFO498f
+summarized_data <- dataset %>% 
+  group_by(What.is.your.programming.experience.) %>% 
+  summarise(
+    num_students = n()
+  )
+
+plot_ly(summarized_data,
+        x = What.is.your.programming.experience., 
+        y = num_students,
+        name = "Programming Experience",
+        type = "bar",
+        color = What.is.your.programming.experience.
+) %>% layout(
+  barmode = "group",
+  bargap = 0.1,
+  title = "INFO498f - Program Experience Level",
+  xaxis = list(title = "Program Experience Level"),
+  yaxis = list(title = "Number of Students")
+)
+
