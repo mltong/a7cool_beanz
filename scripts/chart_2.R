d2040a57716c0177b8efbeee573291a692a08cab
Julie <engjulie@uw.edu>
2016-02-24 11:44:15 -0800

    fix chart_2

--- scripts/chart_2.R
+++ scripts/chart_2.R
@@ -1,4 +1,3 @@
-library(jsonlite)
 library(plotly)
 library(dplyr)
 
@@ -10,23 +9,18 @@ chart_2 <- function(data) {
   data <- read.csv("https://raw.githubusercontent.com/INFO-498F/a7-survey-data/master/intro_survey_data.csv")
   
   # Group the students by their programming experience
-  summarized_data <- select(data) %>% 
-    group_by(data$What.is.your.programming.experience.) %>% 
-    summarise(
-    num_students = n()
-)
+  program_experience_data <- data %>% select(What.is.your.programming.experience.)
+  names(program_experience_data) <- c("program_experience")
+  summarized_data <- program_experience_data %>% group_by(program_experience) %>% 
+    summarise(num_students = n())
   
-plot_ly(summarized_data,
-        x = What.is.your.programming.experience., 
-        y = num_students,
-        name = "Programming Experience",
-        type = "bar",
-        color = "Red"
-) %>% layout(
-  barmode = "group",
-  bargap = 0.1,
-  title = "INFO498f - Program Experience Level",
-  xaxis = list(title = "Program Experience Level"),
-  yaxis = list(title = "Number of Students")
-)
+  plot_ly(summarized_data, x = program_experience,
+          y = num_students,
+          name = "Programming Experience",
+          type = "bar"
+          ) %>% 
+    layout(color = "orange",
+           title = "INFO498f - Program Experience Level", 
+            xaxis = list(title = "Program Experience Level"),
+            yaxis = list(title = "Number of Students"))
 }
